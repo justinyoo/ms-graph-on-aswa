6e1e628f6df436ab0af8e6aa32af067203b84c38
Justin Yoo <justin.yoojh@gmail.com>
2023-03-06 06:43:04 +0000

    Update devcontainer settings

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -62,7 +62,7 @@ git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/cust
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-# git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k --depth=1
+git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k --depth=1
 ln -s $HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme $HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
 
 ## OH-MY-ZSH - POWERLEVEL10K SETTINGS ##
